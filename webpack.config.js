4a30606bc1aebd510c5273278b8bda8bb38d8125
Dawid Kozak <d.kozak92@gmail.com>
2016-07-08 14:00:00 +0200

    Add eslint for webpack and eslint conf from syncano

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel?presets[]=es2015,presets[]=react,presets[]=stage-0,plugins[]=react-html-attrs,plugins[]=transform-class-properties,plugins[]=transform-decorators-legacy']
+        loaders: ['react-hot', 'eslint-loader', 'babel?presets[]=es2015,presets[]=react,presets[]=stage-0,plugins[]=react-html-attrs,plugins[]=transform-class-properties,plugins[]=transform-decorators-legacy']
       },
       {
         test: /\.html$/,
